b3bc4f9ecc6ea5fc1692c150e1df220bdddc0643
kececirecep <kececirecep@outlook.com>
2024-05-21 21:43:29 +0300

    devam edilecek framer animate yapılıyor

--- src/components/ProjectCard/ProjectCard.jsx
+++ src/components/ProjectCard/ProjectCard.jsx
@@ -1,17 +1,22 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
+import { motion } from "framer-motion";
+
+
 const ProjectCard = (props) => {
   return (
-    <div className='shadow rounded-2xl overflow-hidden dark:border dark:border-gray-500'>
+    <motion.div
+    
+     className='shadow rounded-2xl overflow-hidden dark:border dark:border-gray-500'>
       <Link to={props.url} target='_blank'>
-        <div className='w-full'>
-          <img src={props.img} className='w-full h-44 object-cover' alt="" />
+        <div className='w-full h-[276px]'>
+          <img src={props.img} className='w-full h-[276px] object-cover md:object-cover' alt="" />
         </div>
       </Link>
       <div className='p-4'>
         <Link to={props.url} target='_blank' className='text-lg font-semibold mb-2 dark:text-gray-300'>{props.title}</Link>
-        <p className='text-gray-500 text-sm line-clamp-2 dark:text-gray-400'>{props.desc}</p>
+        <p className='text-gray-500 text-sm dark:text-gray-400'>{props.desc}</p>
 
         <div className="flex flex-wrap gap-2 mt-4">
           {
@@ -23,7 +28,7 @@ const ProjectCard = (props) => {
           }
         </div>
       </div>
-    </div>
+    </motion.div>
   )
 }
 
--- src/components/TabMenu/TabMenu.jsx
+++ src/components/TabMenu/TabMenu.jsx
@@ -6,6 +6,8 @@ import { RiCss3Line } from "react-icons/ri";
 import { FaWordpressSimple } from "react-icons/fa";
 
 
+import { motion } from "framer-motion";
+
 const TabMenu = () => {
 
   const projects = [
@@ -17,25 +19,81 @@ const TabMenu = () => {
       url: "https://resao.netlify.app"
     },
     {
-      image: "/images/project1.jpg",
-      title: "Project 2",
-      desc: "Deneme projesi test aşamasında şu anda. React kullanılıyor",
-      tags: ["react", "typescript", "tailwindcss"],
-      url: "#"
+      image: "https://i.imgur.com/WSxKlNL.png",
+      title: "Hekto E-commerce Theme",
+      desc: "React ve Tailwindcss kullanarak geliştirmiş olduğum e-commerce react uygulaması.",
+      tags: ["react", "redux toolkit", "tailwindcss"],
+      url: "https://hekto-creative.netlify.app"
     },
     {
-      image: "/images/project1.jpg",
-      title: "Project 2",
-      desc: "Deneme projesi test aşamasında şu anda. React kullanılıyor",
-      tags: ["s", "css", "tailwindcss"],
-      url: "#"
+      image: "https://i.imgur.com/BLyy1Zi.png",
+      title: "Lawyer Landing Page",
+      desc: "React ve tailwindcss kullanarak geliştirmiş olduğum full responsive bir react projesi.",
+      tags: ["react", "tailwindcss"],
+      url: "https://lawyer-creative-page.netlify.app"
     },
     {
-      image: "/images/project1.jpg",
-      title: "Project 2",
-      desc: "Deneme projesi test aşamasında şu anda. React kullanılıyor",
-      tags: ["a", "typescript", "wordpress"],
-      url: "#"
+      image: "https://i.imgur.com/g45POw4.png",
+      title: "Argencis Landing Page",
+      desc: "React ve tailwindcss kullanarak geliştirmiş olduğum full responsive bir react projesi.",
+      tags: ["react", "tailwindcss", "landing page"],
+      url: "https://argencis-creative.netlify.app"
+    },
+    {
+      image: "https://i.imgur.com/HXxKCx1.png",
+      title: "Creativo",
+      desc: "React ve tailwindcss kullanarak geliştirmiş olduğum full responsive bir react projesi.",
+      tags: ["react", "tailwindcss", "creative page"],
+      url: "https://design-creative.netlify.app"
+    },
+    {
+      image: "https://i.imgur.com/MiuFtqF.png",
+      title: "Banking Landing Page",
+      desc: "React ve tailwindcss kullanarak geliştirmiş olduğum full responsive bir react projesi.",
+      tags: ["react", "tailwindcss", "landing page"],
+      url: "https://banking-landing-page.netlify.app"
+    },
+    {
+      image: "https://i.imgur.com/FQPgfu7.png",
+      title: "Blog Template",
+      desc: "React ve tailwindcss kullanarak geliştirmiş olduğum full responsive bir react projesi.",
+      tags: ["react", "tailwindcss", "blog"],
+      url: "https://kececi-portfolio-blog.netlify.app"
+    },
+    {
+      image: "https://i.imgur.com/K9qsBgM.png",
+      title: "Youtube Video List",
+      desc: "React ve tailwindcss kullanarak geliştirmiş olduğum full responsive bir react projesi.",
+      tags: ["react", "tailwindcss"],
+      url: "https://kececi-youtube.netlify.app"
+    },
+    {
+      image: "https://i.imgur.com/GL1nqJN.png",
+      title: "Todo App",
+      desc: "React ve tailwindcss kullanarak geliştirmiş olduğum full responsive bir react projesi.",
+      tags: ["react", "tailwindcss"],
+      url: "https://kececi-todo.netlify.app"
+    },
+    {
+      image: "https://i.imgur.com/l4VHgxh.png",
+      title: "Portfolio",
+      desc: "React ve tailwindcss kullanarak geliştirmiş olduğum bir react projesi.",
+      tags: ["react", "tailwindcss"],
+      url: "https://kececi-portfolio-v2.netlify.app"
+    },
+    {
+      image: "https://i.imgur.com/zuGTlR1.png",
+      title: "Number Matching Game",
+      desc: "React ve tailwindcss kullanarak geliştirmiş olduğum numara eşleştirme oyunu.",
+      tags: ["react", "tailwindcss"],
+      url: "https://kececi-number-matching-game.netlify.app"
+    },
+    {
+      image: "https://i.imgur.com/1dpkbu5.png",
+      title: "Portfolio v2",
+      desc: "React ve tailwindcss kullanarak geliştirmiş olduğum bir portfolio sitesi.",
+      tags: ["react", "tailwindcss"],
+      url: "https://kececi-number-matching-game.netlify.app"
     }
   ]
 
@@ -50,6 +108,27 @@ const TabMenu = () => {
     const categoryLength = projects.filter(item => item.tags.includes(name))
     return categoryLength.length
   }
+
+
+  const container = {
+    hidden: { opacity: 1, scale: 0 },
+    visible: {
+      opacity: 1,
+      scale: 1,
+      transition: {
+        delayChildren: 0.3,
+        staggerChildren: 0.2
+      }
+    }
+  };
+
+  const item = {
+    hidden: { x: 20, opacity: 0 }, // Yatay kaydırma (x)
+    visible: {
+      x: 0,
+      opacity: 1
+    }
+  };
 
 
   return (
@@ -68,20 +147,27 @@ const TabMenu = () => {
           <button onClick={(e) => reactClick("wordpress")} className={`py-2 px-3 text-sm shadow rounded-xl flex items-center gap-2 dark:text-white border dark:border-[#34495E] ${activeButton === "wordpress" ? "active" : ""}`}><FaWordpressSimple size={"22px"} />Wordpress ({counterCategory("wordpress")})</button>
         </div>
 
-        <div className='grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3 gap-y-6 grid-cols-1 '>
+        <div
+          className='grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-3 gap-y-6 grid-cols-1 items-stretch'>
           {
             projects
               .filter((project) => project.tags.includes(activeButton))
               .map((item, index) => {
                 return (
-                  <ProjectCard
-                    key={index}
-                    img={item.image}
-                    title={item.title}
-                    desc={item.desc}
-                    tags={item.tags}
-                    url={item.url}
-                  />
+                  <motion.div
+                    variants={container}
+                    initial="hidden"
+                    animate="visible"
+                  >
+                    <ProjectCard
+                      key={index}
+                      img={item.image}
+                      title={item.title}
+                      desc={item.desc}
+                      tags={item.tags}
+                      url={item.url}
+                    />
+                  </motion.div>
                 )
               })
           }
